e78c8c79cc640bcda14b244b3a50fa900a204a6a
Patrick-Clausen <60315389+Patrick-Clausen@users.noreply.github.com>
2020-10-07 09:25:48 +0200

    Project setup, Jenkins test

---
+++ Ladeskab/Ladeskab.UnitTests/BasicTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ladeskab.UnitTests
+{
+    [TestFixture]
+    public class BasicTestFixture
+    {
+        [Test]
+        public void AlwaysPassTest()
+        {
+            Assert.That(1,Is.EqualTo(1));
+        }
+    }
+}
